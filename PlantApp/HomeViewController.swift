ae2c3357ac3b91b596d38df0a5950e50c8f97c86
Ravichandrane <rajendran.ravichandrane@gmail.com>
2015-12-14 15:43:40 +0100

    first commit

---
+++ PlantApp/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  PlantApp
+//
+//  Created by Ravichandrane Rajendran on 14/12/15.
+//  Copyright © 2015 Ravichandrane Rajendran. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
